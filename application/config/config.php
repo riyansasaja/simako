f09bdcb6826c2e2ba1eb6710756072b7b8bc85ab
riyansasaja <riyanboyuhu@gmail.com>
2021-06-06 11:43:25 +0800

    sesuaikan dengan server terbaru

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/simako/';
+$config['base_url'] = 'http://ibrizal.site/simako/';
 
 /*
 |--------------------------------------------------------------------------
