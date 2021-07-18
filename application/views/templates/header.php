17c2e8bb2255552efd0665a132e614e8e32e71c6
riyansasaja <riyanboyuhu@gmail.com>
2021-07-18 20:35:11 +0800

    Memasukkan animate css di template header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -15,10 +15,14 @@
     <link href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
+    <!-- animate css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets/') ?>css/sb-admin-2.min.css" rel="stylesheet">
     <link href="<?= base_url('assets/') ?>vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
     <link rel="shortcut icon" href="<?= base_url('assets/img/') ?>international.svg">
+
 
 </head>
 
